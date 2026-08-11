a4c909e5bd7c8649a9c27c4644655737404f29b5
Emran <Dell@MAHMOUD10>
2026-08-11 23:40:50 +0300

    Update 3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,6 +51,8 @@
             this.btnNew = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.lblTaxrate = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbltotalfinal = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPriceItem)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
@@ -59,10 +61,9 @@
             // btnPrint
             // 
             this.btnPrint.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnPrint.Location = new System.Drawing.Point(57, 348);
-            this.btnPrint.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrint.Location = new System.Drawing.Point(43, 283);
             this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(217, 78);
+            this.btnPrint.Size = new System.Drawing.Size(163, 63);
             this.btnPrint.TabIndex = 0;
             this.btnPrint.Text = "طباعة الفاتورة";
             this.btnPrint.UseVisualStyleBackColor = true;
@@ -70,13 +71,13 @@
             // btnAdd
             // 
             this.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnAdd.Location = new System.Drawing.Point(361, 348);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Location = new System.Drawing.Point(271, 283);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(217, 78);
+            this.btnAdd.Size = new System.Drawing.Size(163, 63);
             this.btnAdd.TabIndex = 1;
             this.btnAdd.Text = "إضافة الصنف";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // cbItems
             // 
@@ -88,10 +89,9 @@
             "ورق A3 80 gm",
             "تغليف سلك",
             "تغليف شطرطون"});
-            this.cbItems.Location = new System.Drawing.Point(735, 129);
-            this.cbItems.Margin = new System.Windows.Forms.Padding(4);
+            this.cbItems.Location = new System.Drawing.Point(551, 105);
             this.cbItems.Name = "cbItems";
-            this.cbItems.Size = new System.Drawing.Size(257, 24);
+            this.cbItems.Size = new System.Drawing.Size(194, 21);
             this.cbItems.TabIndex = 2;
             this.cbItems.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -102,10 +102,9 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(881, 75);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(661, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 36);
+            this.label1.Size = new System.Drawing.Size(73, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "الصنف";
             // 
@@ -116,10 +115,9 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(880, 172);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(660, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 36);
+            this.label2.Size = new System.Drawing.Size(62, 29);
             this.label2.TabIndex = 4;
             this.label2.Text = "الكمية";
             // 
@@ -130,18 +128,16 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(829, 267);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(622, 217);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(121, 36);
+            this.label3.Size = new System.Drawing.Size(100, 29);
             this.label3.TabIndex = 5;
             this.label3.Text = "سعر الحبة";
             // 
             // numQuantity
             // 
             this.numQuantity.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.numQuantity.Location = new System.Drawing.Point(736, 226);
-            this.numQuantity.Margin = new System.Windows.Forms.Padding(4);
+            this.numQuantity.Location = new System.Drawing.Point(552, 184);
             this.numQuantity.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -153,7 +149,7 @@
             0,
             0});
             this.numQuantity.Name = "numQuantity";
-            this.numQuantity.Size = new System.Drawing.Size(257, 22);
+            this.numQuantity.Size = new System.Drawing.Size(193, 20);
             this.numQuantity.TabIndex = 8;
             this.numQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numQuantity.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
@@ -169,8 +165,7 @@
             this.numPriceItem.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.numPriceItem.DecimalPlaces = 2;
             this.numPriceItem.InterceptArrowKeys = false;
-            this.numPriceItem.Location = new System.Drawing.Point(736, 334);
-            this.numPriceItem.Margin = new System.Windows.Forms.Padding(4);
+            this.numPriceItem.Location = new System.Drawing.Point(552, 271);
             this.numPriceItem.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -182,16 +177,16 @@
             0,
             131072});
             this.numPriceItem.Name = "numPriceItem";
-            this.numPriceItem.Size = new System.Drawing.Size(257, 22);
+            this.numPriceItem.Size = new System.Drawing.Size(193, 20);
             this.numPriceItem.TabIndex = 9;
             this.numPriceItem.Tag = "0";
             this.numPriceItem.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numPriceItem.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
             this.numPriceItem.Value = new decimal(new int[] {
-            1,
+            50,
             0,
             0,
-            0});
+            131072});
             this.numPriceItem.ValueChanged += new System.EventHandler(this.numPriceItem_ValueChanged);
             // 
             // dgvData
@@ -224,8 +219,7 @@
             this.dgvData.DefaultCellStyle = dataGridViewCellStyle8;
             this.dgvData.EnableHeadersVisualStyles = false;
             this.dgvData.GridColor = System.Drawing.Color.Black;
-            this.dgvData.Location = new System.Drawing.Point(97, 37);
-            this.dgvData.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvData.Location = new System.Drawing.Point(73, 30);
             this.dgvData.Name = "dgvData";
             this.dgvData.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.dgvData.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
@@ -242,7 +236,7 @@
             dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle10.BackColor = System.Drawing.Color.Gainsboro;
             this.dgvData.RowsDefaultCellStyle = dataGridViewCellStyle10;
-            this.dgvData.Size = new System.Drawing.Size(419, 214);
+            this.dgvData.Size = new System.Drawing.Size(314, 174);
             this.dgvData.TabIndex = 10;
             // 
             // الصنف
@@ -255,38 +249,37 @@
             this.الصنف.MinimumWidth = 6;
             this.الصنف.Name = "الصنف";
             this.الصنف.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.الصنف.Width = 82;
+            this.الصنف.Width = 67;
             // 
             // الكمية
             // 
             this.الكمية.HeaderText = "الكمية";
             this.الكمية.MinimumWidth = 6;
             this.الكمية.Name = "الكمية";
-            this.الكمية.Width = 76;
+            this.الكمية.Width = 67;
             // 
             // سعر
             // 
             this.سعر.HeaderText = "سعر الحبة";
             this.سعر.MinimumWidth = 6;
             this.سعر.Name = "سعر";
-            this.سعر.Width = 103;
+            this.سعر.Width = 88;
             // 
             // Total
             // 
             this.Total.HeaderText = "الإجمالي";
             this.Total.MinimumWidth = 6;
             this.Total.Name = "Total";
-            this.Total.Width = 92;
+            this.Total.Width = 81;
             // 
             // label4
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(876, 402);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(657, 327);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 31);
+            this.label4.Size = new System.Drawing.Size(76, 25);
             this.label4.TabIndex = 11;
             this.label4.Text = "الإحمالي";
             // 
@@ -295,20 +288,18 @@
             this.lblTotal.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblTotal.AutoSize = true;
             this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.Location = new System.Drawing.Point(728, 402);
-            this.lblTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotal.Location = new System.Drawing.Point(546, 327);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(30, 31);
+            this.lblTotal.Size = new System.Drawing.Size(25, 25);
             this.lblTotal.TabIndex = 12;
             this.lblTotal.Text = "0";
             // 
             // btnNew
             // 
             this.btnNew.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnNew.Location = new System.Drawing.Point(239, 462);
-            this.btnNew.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNew.Location = new System.Drawing.Point(179, 375);
             this.btnNew.Name = "btnNew";
-            this.btnNew.Size = new System.Drawing.Size(217, 78);
+            this.btnNew.Size = new System.Drawing.Size(163, 63);
             this.btnNew.TabIndex = 13;
             this.btnNew.Text = "فاتورة جديدة";
             this.btnNew.UseVisualStyleBackColor = true;
@@ -318,30 +309,53 @@
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(809, 462);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(607, 375);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(225, 31);
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(181, 25);
             this.label6.TabIndex = 14;
-            this.label6.Text = "شامل الضريبة % 15";
+            this.label6.Text = "شامل الضريبة 15 %";
             // 
             // lblTaxrate
             // 
             this.lblTaxrate.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblTaxrate.AutoSize = true;
             this.lblTaxrate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTaxrate.Location = new System.Drawing.Point(727, 462);
-            this.lblTaxrate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTaxrate.Location = new System.Drawing.Point(545, 375);
             this.lblTaxrate.Name = "lblTaxrate";
-            this.lblTaxrate.Size = new System.Drawing.Size(30, 31);
+            this.lblTaxrate.Size = new System.Drawing.Size(25, 25);
             this.lblTaxrate.TabIndex = 15;
             this.lblTaxrate.Text = "0";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Al-Jazeera-Arabic-Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(314, 207);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(78, 27);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "الإجمالي :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbltotalfinal
+            // 
+            this.lbltotalfinal.AutoSize = true;
+            this.lbltotalfinal.Font = new System.Drawing.Font("Al-Jazeera-Arabic-Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotalfinal.Location = new System.Drawing.Point(219, 207);
+            this.lbltotalfinal.Name = "lbltotalfinal";
+            this.lbltotalfinal.Size = new System.Drawing.Size(0, 27);
+            this.lbltotalfinal.TabIndex = 17;
+            this.lbltotalfinal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltotalfinal);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.lblTaxrate);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.btnNew);
@@ -356,9 +370,9 @@
             this.Controls.Add(this.cbItems);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnPrint);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPriceItem)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
@@ -387,6 +401,8 @@
         private System.Windows.Forms.Button btnNew;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label lblTaxrate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbltotalfinal;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,26 +15,27 @@ namespace Bills
     public partial class Form1 : Form
     {
         private string _ItemName;
+        
         Dictionary<string, float> price = new Dictionary<string, float>();
         private void AddtoData()
         {
-            price.Add("ورق A4 80 gm", 1);
-            price.Add("ورق مقوى A4 180 gm", 3);
-            price.Add("ورق A3 80 gm", 4);
-            price.Add("تغليف سلك", 5);
-            price.Add("تغليف شطرطون", 3);
+            price.Add("ورق A4 80 gm", 0.85f);
+            price.Add("ورق مقوى A4 180 gm", 2.70f);
+            price.Add("ورق A3 80 gm", 3.48f);
+            price.Add("تغليف سلك", 4.35f);
+            price.Add("تغليف شطرطون", 2.61f);
         }
       
         public Form1()
         {
-           
+          
             InitializeComponent();
             AddtoData();
         }
 
-        private float GetPriceItem(string Item)
+        private float GetPriceItem()
         {
-            if(price.ContainsKey(Item)) return price[Item];
+            if(price.ContainsKey(_ItemName)) return price[_ItemName];
             return 0;
             
         }
@@ -44,37 +45,77 @@ namespace Bills
         }
         private float GetTaxrate()
         {
-            return  Convert.ToSingle (GetPriceTotal() * 0.15);
+            float TaxRate = 0.15f;
+            return   TaxRate;
         }
         private float GetPriceTotalwithTaxrate()
         {
-            return GetPriceTotal() + GetTaxrate();
+            return   GetTaxrate()* GetPriceTotal();
         }
 
-        private void Price()
+        private void UpdatePrice()
         {
-            lblTotal.Text=GetPriceTotal().ToString();
-            lblTaxrate.Text=GetPriceTotalwithTaxrate().ToString();
+            lblTotal.Text=GetPriceTotal().ToString("F2");
+            lblTaxrate.Text=GetPriceTotalwithTaxrate().ToString("F2");
             
         }
   
+        private void UpdateTotalBill()
+        {
+            float TotalBill=0;
+            foreach (DataGridViewRow Row in dgvData.Rows)
+            {
+                TotalBill += Convert.ToSingle(Row.Cells[3].Value);
+            }
+            lbltotalfinal.Text=TotalBill.ToString();
+        }
+        private void AddItemToBill()
+        {
+            string ItemName=_ItemName;
+            decimal price = numPriceItem.Value;
+            decimal Quantity = numQuantity.Value;
+            float Total= (float)Math.Round(GetPriceTotalwithTaxrate(),2);
 
+            dgvData.Rows.Add(ItemName,Quantity, price, Total);
+
+            UpdateTotalBill();
+            Reset();
+        }
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-             
              _ItemName= cbItems.SelectedItem.ToString();
-            Price();
+            numPriceItem.Value = (decimal)GetPriceItem();
         }
 
+        private void Reset()
+        {
+           // cbItems.Items.;
+            
+        }
         private void numQuantity_ValueChanged(object sender, EventArgs e)
         {
-            Price();
+            UpdatePrice();
         }
 
         private void numPriceItem_ValueChanged(object sender, EventArgs e)
         {
            
-            Price();
+            UpdatePrice();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddItemToBill();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
